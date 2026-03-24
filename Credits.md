59cc3e79b92250b46d28b9d25e0a30cd3ef2e107
Ankit Suman <ankitsuman07@gmail.com>
2026-03-23 21:00:45 -0400

    Setup Game screen UI

--- Credits.md
+++ Credits.md
@@ -7,3 +7,13 @@
 - cherries: [Link](<a href="https://www.flaticon.com/free-icons/cherry-tomato" title="cherry tomato icons">Cherry tomato icons created by Icon home - Flaticon</a>)
 - wild card: [Link](<a href="https://www.flaticon.com/free-icons/wildcard" title="wildcard icons">Wildcard icons created by Flat Icons - Flaticon</a>)
 - star: [Link](<a href="https://www.flaticon.com/free-icons/star" title="star icons">Star icons created by Freepik - Flaticon</a>)
+- close: [Link](<a href="https://www.flaticon.com/free-icons/close" title="close icons">Close icons created by Arkinasi - Flaticon</a>)
+- restart: [Link](<a href="https://www.flaticon.com/free-icons/restart" title="restart icons">Restart icons created by riajulislam - Flaticon</a>)
+
+## Font
+-   Name: Pixel Game
+    -   From [1001fints](https://www.1001fonts.com/pixel-game-font.html)
+    -   By [SuhaDesign](https://www.1001fonts.com/users/suhadidesign/)
+    -   License: Free for personal use
+
+## Music
--- scripts/game_screen.gd
+++ scripts/game_screen.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var mid_slot = $CenterContainer/HBoxContainer/SecondPanel/middle
 @onready var right_slot = $CenterContainer/HBoxContainer/ThirdPanel/right
 
+@onready var winnings_label = $WinningsLabel
+@onready var bet_label = $BetLabel
+
 const SEVEN = preload("res://assets/seven.png")
 const BELL = preload("res://assets/bell.png")
 const LEMON = preload("res://assets/lemon.png")
@@ -20,3 +23,46 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func spin_slot():
+	print("Spining")
+
+func change_image():
+	pass
+
+
+
+func _on_spin_btn_pressed() -> void:
+	spin_slot()
+	
+
+
+
+func _on_bet_5_pressed() -> void:
+	print("5")
+
+
+func _on_bet_25_pressed() -> void:
+	print("25")
+
+
+func _on_bet_50_pressed() -> void:
+	print("50")
+
+
+func _on_bet_100_pressed() -> void:
+	print("100")
+
+
+func _on_bet_250_pressed() -> void:
+	print("250")
+
+
+func _on_restart_btn_pressed() -> void:
+	print("Restart")
+	
+
+
+
+func _on_close_btn_pressed() -> void:
+	print("Close")
